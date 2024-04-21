05c4678f74af41e7d44c7bc0d0619b0461caafd2
DucTrung611 <thanductrung123@gmail.com>
2024-04-21 10:56:36 +0700

    44

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -8,7 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World with hoidanit!";
+        return "Hello World 123!";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only user can access this page";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only admin can access this page";
     }
 
 }
